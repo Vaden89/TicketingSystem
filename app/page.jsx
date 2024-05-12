c6be7ee81d468282b764eb569558c90a7917b3e3
Vaden89 <isaacshosanya89@gmail.com>
2024-05-12 12:42:31 +0100

    remade the sign in page for effective responsiveness and created the frontend for the sign up page

--- app/page.jsx
+++ app/page.jsx
@@ -6,45 +6,56 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="w-64 flex flex-col justify-center items-center gap-8 absolute top-[25%] right-[19%] text-center ">
-      <span className="text-xl text-indigo-500">
-        Sign In To <span className="font-extrabold">DashBoard</span>
-      </span>
-      <span className="w-full border-2 border-pink-300"></span>
-      <div className="flex flex-col items-start gap-2">
-        <span className="text-indigo-500 text-lg">Username</span>
-        <input
-          className="w-full text-lg p-4 h-10 border-2 border-solid border-pink-400  rounded-xl"
-          type="text"
-          name="username"
-          id=""
-        />
-      </div>
-      <div className="flex flex-col items-start gap-2">
-        <span className="text-indigo-500 text-lg">Password</span>
-        <input
-          className="w-full text-lg p-4 h-10 border-2 border-solid border-pink-400 rounded-xl"
-          type="password"
-          name="pwd"
-          id=""
-        />
-      </div>
-      <button
-        type="submit"
-        className="bg-pink-300 w-full h-11 rounded-xl text-white text-xl cursor-pointer"
-      >
-        Sign In
-      </button>
-      <span className="font-light">
-        Don&apos;t have an account?&nbsp;
-        <span
-          onClick={() => {
-            router.push("/signup");
-          }}
-        >
-          Sign Up
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <div className=" flex flex-col items-center justify-center">
+        <h1 className="text-indigo-500 text-xl ">
+          Sign In To <span className="font-bold">Dashboard </span>
+        </h1>
+        <span className="w-full lg:w-80 border-2 border-pink-300 mt-4 "></span>
+        <form className="mt-4" action="">
+          <div className="flex flex-col ">
+            <label htmlFor="username" className="text-indigo-500 text-lg">
+              Username:
+            </label>
+            <input
+              className="w-full lg:w-80 text-lg p-4 h-10 border-2 border-solid border-pink-400 mt-1 rounded-xl"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Your Username"
+            />
+          </div>
+
+          <div className="flex flex-col mt-3">
+            <label htmlFor="pwd" className="text-indigo-500 text-lg">
+              Password:
+            </label>
+            <input
+              className="w-full lg:w-80 text-lg sp-4 h-10 border-2 border-solid border-pink-400 mt-1 rounded-xl"
+              type="password"
+              name="pwd"
+              id="pwd"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-pink-300 w-full h-11 rounded-xl text-white text-xl cursor-pointer hover:scale-105 btn-transition mt-4"
+          >
+            Sign In
+          </button>
+        </form>
+        <span className="font-light mt-4">
+          Already have an account?&nbsp;
+          <span
+            className="cursor-pointer text-indigo-500 hover:font-semibold"
+            onClick={() => {
+              router.push("/signup");
+            }}
+          >
+            Sign Up
+          </span>
         </span>
-      </span>
+      </div>
     </div>
   );
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,3 +1,69 @@
+"use client";
+import { useRouter } from "next/navigation";
 export default function Signup() {
-  return <div>Isaac</div>;
+  const router = useRouter();
+  return (
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <div className=" flex flex-col items-center justify-center">
+        <h1 className="text-indigo-500 text-xl ">
+          Sign Up For <span className="font-bold">Dashboard </span>
+        </h1>
+        <span className="w-full lg:w-80 border-2 border-pink-300 mt-4 "></span>
+        <form className="mt-4" action="">
+          <div className="flex flex-col ">
+            <label htmlFor="username" className="text-indigo-500 text-lg">
+              Username:
+            </label>
+            <input
+              className="w-full lg:w-80 text-lg p-4 h-10 border-2 border-solid border-pink-400 mt-1 rounded-xl"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Your Username"
+            />
+          </div>
+          <div className="flex flex-col mt-3">
+            <label className="text-indigo-500 text-lg" htmlFor="email">
+              Email :
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="w-full lg:w-80 text-lg p-4 h-10 border-2 border-solid border-pink-400 mt-1 rounded-xl"
+              placeholder="example@mail.com"
+            />
+          </div>
+          <div className="flex flex-col mt-3">
+            <label htmlFor="pwd" className="text-indigo-500 text-lg">
+              Password:
+            </label>
+            <input
+              className="w-full lg:w-80 text-lg sp-4 h-10 border-2 border-solid border-pink-400 mt-1 rounded-xl"
+              type="password"
+              name="pwd"
+              id="pwd"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-pink-300 w-full h-11 rounded-xl text-white text-xl cursor-pointer hover:scale-105 btn-transition mt-4"
+          >
+            Sign Up
+          </button>
+        </form>
+        <span className="font-light mt-4">
+          Already have an account?&nbsp;
+          <span
+            className="cursor-pointer text-indigo-500 hover:font-semibold"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            Sign In
+          </span>
+        </span>
+      </div>
+    </div>
+  );
 }
